37bf1475ef477ecc2566b1428bd5d6edad6a580f
maggie <mmo34985@uga.edu>
2022-04-20 14:53:26 -0400

    Basic layout for Purchased List screen.

---
+++ app/src/main/java/edu/uga/cs/finalproject_sharedshoppinglist/PurchasedItemsListActivity.java
@@ -0,0 +1,14 @@
+package edu.uga.cs.finalproject_sharedshoppinglist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PurchasedItemsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_purchased_items_list);
+    }
+}
